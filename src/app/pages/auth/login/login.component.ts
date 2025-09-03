3fe305dbe8dc968caede37e4512616c70c3335f8
gabrielpanta <gabrielpantajimenez@hotmail.com>
2025-09-02 20:24:14 -0500

    Validar formulario Login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule,Validator, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import DashboardComponent from '../../dashboard/dashboard.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,DashboardComponent,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export  class LoginComponent {
 
-}
+    loginForm = this.formBuilder.group({
+        username: ['encargado', [Validators.required]],
+        password: ['123456',[Validators.required]]  
+    });
+
+    constructor(private formBuilder: FormBuilder, private router:Router) { }
+
+    get username() {
+        return this.loginForm.controls.username;
+    }
+
+    get password() {
+        return this.loginForm.controls.password;
+    }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log("Formulario válido");
+      this.router.navigateByUrl('/');
+      this.loginForm.reset();
+    } else {
+      alert("Formulario inválido");
+    }
+  }
+
+  }
